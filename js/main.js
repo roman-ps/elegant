a88109aa3006fdcc906bb50d6ddf08b7dc032906
Pozharov Roman <ru.roman.ps@gmail.com>
2018-05-10 19:18:59 +0400

    Доработан слайдер-карусель

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var sliders = document.querySelector(".services__list");
+var btnLeft = document.querySelector(".sliders__btn-left");
+var btnRight = document.querySelector(".sliders__btn-right");
+
+btnRight.addEventListener("click", function(event) {
+  event.preventDefault();
+  sliders.style.marginLeft = "-1170px";
+  btnRight.classList.add("sliders__btn--active");
+  btnLeft.classList.remove("sliders__btn--active");
+});
+
+btnLeft.addEventListener("click", function(event) {
+  event.preventDefault();
+  sliders.style.marginLeft = "0px";
+  btnRight.classList.remove("sliders__btn--active");
+  btnLeft.classList.add("sliders__btn--active");
+});
