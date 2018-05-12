5544ff6d745726e3c8c034d9fe4bae207953bef5
Pozharov Roman <ru.roman.ps@gmail.com>
2018-05-12 16:14:55 +0400

    Реализовано поведение кнопок и дополнительных элементов списка

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 var sliders = document.querySelector(".services__list");
 var btnLeft = document.querySelector(".sliders__btn-left");
 var btnRight = document.querySelector(".sliders__btn-right");
+var btnMore = document.querySelector(".btn--work-open");
+var btnClose = document.querySelector(".btn--work-close");
+var workItemAdd = document.querySelectorAll(".work__list-item--additional");
+var i;
 
 btnRight.addEventListener("click", function(event) {
   event.preventDefault();
@@ -16,4 +20,22 @@ btnLeft.addEventListener("click", function(event) {
   sliders.style.marginLeft = "0px";
   btnRight.classList.remove("sliders__btn--active");
   btnLeft.classList.add("sliders__btn--active");
+});
+
+btnMore.addEventListener("click", function(event) {
+  event.preventDefault();
+  btnMore.classList.add("hide");
+  for (i=0; i < workItemAdd.length; i++) {
+    workItemAdd[i].classList.add("open");
+  }
+  btnClose.classList.remove("hide");
+});
+
+btnClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  btnClose.classList.add("hide");
+  for (i=0; i < workItemAdd.length; i++) {
+    workItemAdd[i].classList.remove("open");
+  }
+  btnMore.classList.remove("hide");
 });
