34aa498435e7267fe6b87d9c0485aec10239c78c
Pozharov Roman <ru.roman.ps@gmail.com>
2018-05-13 21:15:00 +0400

    Рабочая сортировка по data-name=branding

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var btnMore = document.querySelector(".btn--work-open");
 var btnClose = document.querySelector(".btn--work-close");
 var workItemAdd = document.querySelectorAll(".work__list-item--additional");
 var i;
+var jsClick = document.querySelector(".js-click");
+var workItem = document.querySelectorAll(".work__list-item");
+
 
 btnRight.addEventListener("click", function(event) {
   event.preventDefault();
@@ -38,4 +41,16 @@ btnClose.addEventListener("click", function(event) {
     workItemAdd[i].classList.remove("open");
   }
   btnMore.classList.remove("hide");
+});
+
+jsClick.addEventListener("click", function(event) {
+  event.preventDefault();
+  var new1 = jsClick.dataset.name;
+  for (i = 0; i < workItem.length; i++) {
+    if (new1 == workItem[i].getAttribute("data-name")) {
+      console.log(workItem[i]);
+      workItem[i].style.display = "block";
+    } else
+      workItem[i].style.display = "none";
+  }
 });
