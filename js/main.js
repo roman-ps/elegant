f71e14e779282486a74478fc761f3e8750ea4bd5
Pozharov Roman <ru.roman.ps@gmail.com>
2018-07-19 10:00:25 +0400

    Почистил js код

--- js/main.js
+++ js/main.js
@@ -6,13 +6,12 @@ var btnRight = document.querySelector(".sliders__btn-right");
 var btnMore = document.querySelector(".btn--work-open");
 var btnClose = document.querySelector(".btn--work-close");
 var workItemAdd = document.querySelectorAll(".work__list-item--additional");
-var i;
-//var jsClick = document.querySelector(".js-click");
-var jsClick1 = document.querySelectorAll(".js-click");
+var jsClick = document.querySelectorAll(".js-click");
 var workItem = document.querySelectorAll(".work__list-item");
 var menuOpen = document.querySelector(".menu__btn--open");
 var menuClose = document.querySelector(".menu__btn--close");
 var menu = document.querySelector(".header__nav");
+var i;
 
 
 btnRight.addEventListener("click", function(event) {
@@ -47,24 +46,10 @@ btnClose.addEventListener("click", function(event) {
   btnMore.classList.remove("hide");
 });
 
-//for (i = 0; i < jsClick1.length; i++) {
-//jsClick1[i].addEventListener("click", function(event) {
-//  event.preventDefault();
-//  var new1 = jsClick1[i].dataset.name;
-//  console.log(new1);
-//  for (i = 0; i < workItem.length; i++) {
-//    if (new1 === workItem[i].getAttribute("data-name")) {
-//      workItem[i].style.display = "block";
-//    }
-//    else workItem[i].style.display = "none";
-//  }
-//})
-//}
-
-for (i = 0; i < jsClick1.length; i++) {
-jsClick1[i].addEventListener("click", function(event) {
+for (i = 0; i < jsClick.length; i++) {
+jsClick[i].addEventListener("click", function(event) {
   event.preventDefault();
-  var new1 = jsClick1[0].dataset.name;
+  var new1 = jsClick[0].dataset.name;
   console.log(new1);
   for (i = 0; i < workItem.length; i++) {
     if (new1 === workItem[i].getAttribute("data-name")) {
