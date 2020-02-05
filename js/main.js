2bf118c4ea5b62a03d55d5218c696729a68ef9e2
Pozharov Roman <ru.roman.ps@gmail.com>
2020-02-05 20:33:45 +0400

    Вынесены обработчики

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Selectors = {
+const Selector = {
   SLIDERS: '.services__list',
   BTN_LEFT: '.sliders__btn-left',
   BTN_RIGHT: '.sliders__btn-right',
@@ -9,17 +9,14 @@ const Selectors = {
   MENU_OPEN: '.menu__btn--open',
   MENU_CLOSE: '.menu__btn--close',
   MENU: '.header__nav',
+  NAV: '.work__nav',
 }
 
-const SelectorsAll = {
+const Selectors = {
   ITEM_ADD: '.work__list-item--additional',
   ITEM: '.work__list-item',
   JSCLICK: '.js-click',
 }
-
-let workItemAdd = document.querySelectorAll(".work__list-item--additional");
-let jsClick = document.querySelectorAll(".js-click");
-let workItem = document.querySelectorAll(".work__list-item");
 
 function toNodes(Selectors, flag){
   let key = Object.keys(Selectors);
@@ -30,69 +27,81 @@ function toNodes(Selectors, flag){
   return Temp;
 }
 
-const Node = toNodes(Selectors, );
-const Nodes = toNodes(SelectorsAll, true);
+const Node = toNodes(Selector, );
+const Nodes = toNodes(Selectors, true);
 
 document.addEventListener("DOMContentLoaded", toNodes);
+Node.BTN_LEFT.addEventListener("click", sliderMoveLeft);
+Node.BTN_RIGHT.addEventListener("click", sliderMoveRight);
+Node.BTN_MORE.addEventListener("click", openAdditionalItems);
+Node.BTN_CLOSE.addEventListener("click", closeAdditionalItems);
+Node.MENU_OPEN.addEventListener("click", openMenu);
+Node.MENU_CLOSE.addEventListener("click", closeMenu);
+Node.NAV.addEventListener("click", handlerNavigation);
 
-Node.BTN_RIGHT.addEventListener("click", function(evt) {
+function sliderMoveRight(evt) {
   evt.preventDefault();
   Node.SLIDERS.style.transform = "translateX(-100%)";
   Node.BTN_RIGHT.classList.add("sliders__btn--active");
   Node.BTN_LEFT.classList.remove("sliders__btn--active");
-});
+};
 
-Node.BTN_LEFT.addEventListener("click", function(evt) {
+function sliderMoveLeft(evt) {
   evt.preventDefault();
   Node.SLIDERS.style.transform = "translateX(0px)";
   Node.BTN_RIGHT.classList.remove("sliders__btn--active");
   Node.BTN_LEFT.classList.add("sliders__btn--active");
-});
+};
 
-Node.BTN_MORE.addEventListener("click", function(evt) {
+function openAdditionalItems(evt) {
   evt.preventDefault();
   Node.BTN_MORE.classList.add("hide");
   for (let i = 0; i < Nodes.ITEM_ADD.length; i++) {
     Nodes.ITEM_ADD[i].classList.add("open");
   }
   Node.BTN_CLOSE.classList.remove("hide");
-});
+};
 
-Node.BTN_CLOSE.addEventListener("click", function(evt) {
+function closeAdditionalItems(evt) {
   evt.preventDefault();
   Node.BTN_CLOSE.classList.add("hide");
   for (let i = 0; i < Nodes.ITEM_ADD.length; i++) {
     Nodes.ITEM_ADD[i].classList.remove("open");
   }
   Node.BTN_MORE.classList.remove("hide");
-});
+};
 
-for (let i = 0; i < Nodes.JSCLICK.length; i++) {
-  Nodes.JSCLICK[i].addEventListener("click", function(evt) {
+function handlerNavigation(evt){
   evt.preventDefault();
-  var new1 = Nodes.JSCLICK[0].dataset.name;
-  for (let i = 0; i < Nodes.ITEM.length; i++) {
-    if (new1 === Nodes.ITEM[i].getAttribute("data-name")) {
-      Nodes.ITEM[i].style.display = "block";
+  let child = evt.target;
+  let parent = evt.currentTarget;
+  let temp = evt.target.dataset.name;
+  if (child != parent && temp != '') {
+    for (let i = 0; i < Nodes.ITEM.length; i++) {
+      if (temp == Nodes.ITEM[i].dataset.name) {
+        Nodes.ITEM[i].style.display = "block";
+      }
+      else Nodes.ITEM[i].style.display = "none";
     }
-    else Nodes.ITEM[i].style.display = "none";
   }
+  if (temp == 'all') {
+    for (let i = 0; i < Nodes.ITEM.length; i++) {
+      Nodes.ITEM[i].style.display = "block";
+      Nodes.ITEM_ADD[i].style.display = "none";
+    }
   }
-                             )
-}
+};
 
-Node.MENU_OPEN.addEventListener("click", function(evt) {
+function openMenu(evt) {
   evt.preventDefault();
   Node.MENU_OPEN.classList.add("menu__btn-hide");
   Node.MENU_CLOSE.classList.add("menu__btn-show");
   Node.MENU.classList.add("open");
-});
+};
 
-Node.MENU_CLOSE.addEventListener("click", function(evt) {
+function closeMenu(evt) {
   evt.preventDefault();
   Node.MENU_CLOSE.classList.remove("menu__btn-show");
   Node.MENU_OPEN.classList.remove("menu__btn-hide");
   Node.MENU.classList.remove("open");
-});
-
-
+};
